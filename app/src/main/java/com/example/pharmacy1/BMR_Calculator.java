210148e4346ffe18053110d929d26d1d401f4120
94772 <rashmiraveena521@gmail.com>
2020-10-03 00:33:30 +0530

    Added BMR calculator

--- app/src/main/java/com/example/pharmacy1/BMR_Calculator.java
+++ app/src/main/java/com/example/pharmacy1/BMR_Calculator.java
@@ -10,7 +10,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class BMR_Calculator extends AppCompatActivity {
-
+    Button Wcal;
+    Button Mcal;
+    EditText Mh;
+    EditText Mw;
+    EditText Mage;
+    EditText Wh;
+    EditText Ww;
+    EditText Wage;
+    TextView finM;
+    TextView finW;
+    String MenCal,WomenCal;
 
 
     @Override
@@ -19,6 +29,60 @@ public class BMR_Calculator extends AppCompatActivity {
         setContentView(R.layout.activity_b_m_r__calculator);
 
 
+        Wcal=findViewById(R.id.Wcalcbtn);
+        Mcal=findViewById(R.id.Mcalcbtn);
+        Mh=findViewById(R.id.mheight);
+        Mw=findViewById(R.id.mweight);
+        Mage=findViewById(R.id.mage);
+        Wh=findViewById(R.id.wheight);
+        Ww=findViewById(R.id.wweight);
+        Wage=findViewById(R.id.wage);
+        finM=findViewById(R.id.Mvalue);
+        finW=findViewById(R.id.Wvalue);
+
+        Mcal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String menheight=Mh.getText().toString();
+                String menweight=Mw.getText().toString();
+                String menage=Mage.getText().toString();
+
+
+                float Mheight=Float.parseFloat(menheight);
+                float Mweight=Float.parseFloat(menweight);
+                float Mage=Float.parseFloat(menage);
+                float BMRmen= (float) (66.47+(13.75 * Mweight)+(5.003 * Mheight)-(6.77 * Mage));
+
+
+                finM.setText(String.valueOf(BMRmen));
+
+
+
+
+            }
+        });
+
+
+        Wcal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String womenheight=Mh.getText().toString();
+                String womenweight=Mw.getText().toString();
+                String womenage=Mage.getText().toString();
+
+                float Wheight=Float.parseFloat(womenheight);
+                float Wweight=Float.parseFloat(womenweight);
+                float Wage=Float.parseFloat(womenage);
+                float BMRmen= (float) (651.1+(9.563 * Wweight)+(1.85 * Wheight)-(4.676 * Wage));
+
+                finW.setText(String.valueOf(BMRmen));
+
+
+
+            }
+        });
     }
 
 }
