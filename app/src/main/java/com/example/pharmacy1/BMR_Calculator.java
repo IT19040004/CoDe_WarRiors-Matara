ee515ba41234da3de4367fbaf6b0234053a6557b
94772 <rashmiraveena521@gmail.com>
2020-10-03 00:28:42 +0530

    After adding insert,view,update and delete

---
+++ app/src/main/java/com/example/pharmacy1/BMR_Calculator.java
@@ -0,0 +1,24 @@
+package com.example.pharmacy1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BMR_Calculator extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_m_r__calculator);
+
+
+    }
+
+}
